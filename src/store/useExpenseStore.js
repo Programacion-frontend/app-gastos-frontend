ac5850d32e303364baaee708a2c9478fe4a2f6eb
JOSSEyela <jotay234@gmail.com>
2026-06-11 19:26:30 -0500

    refactor: unificar fetchs en useExpenseStore y agregar utils de moneda

--- src/store/useExpenseStore.js
+++ src/store/useExpenseStore.js
@@ -1,42 +1,39 @@
 import { create } from 'zustand'
 import api from '../utils/axios'
 
-const useExpenseStore = create((set) => ({
+/**
+ * `movimientos` es un arreglo compartido por las páginas de Movimientos,
+ * Ingresos y Gastos. Cada fetch debe dejarlo con SOLO los datos de su tipo.
+ *
+ * Helper que ejecuta una petición y normaliza el estado del arreglo:
+ * - Lo vacía al iniciar (evita mostrar datos de la página anterior).
+ * - El backend devuelve 200 con [] cuando no hay registros, así que basta
+ *   con asignar la respuesta.
+ * - Ante un error real, deja el arreglo vacío y propaga el error.
+ */
+const fetchInto = (set) => async (url, filters) => {
+  set({ isLoading: true, movimientos: [] })
+  try {
+    const { data } = await api.get(url, { params: filters })
+    set({ movimientos: data, isLoading: false })
+  } catch (err) {
+    set({ movimientos: [], isLoading: false })
+    throw err
+  }
+}
+
+const useExpenseStore = create((set) => {
+  const run = fetchInto(set)
+
+  return {
   movimientos: [],
   isLoading:   false,
 
-  fetchMovimientos: async (filters = {}) => {
-    set({ isLoading: true })
-    try {
-      const { data } = await api.get('/movimientos', { params: filters })
-      set({ movimientos: data, isLoading: false })
-    } catch (err) {
-      set({ isLoading: false })
-      throw err
-    }
-  },
+  fetchMovimientos: (filters = {}) => run('/movimientos', filters),
 
-  fetchIngresos: async (filters = {}) => {
-    set({ isLoading: true })
-    try {
-      const { data } = await api.get('/movimientos/ingresos', { params: filters })
-      set({ movimientos: data, isLoading: false })
-    } catch (err) {
-      set({ isLoading: false })
-      throw err
-    }
-  },
+  fetchIngresos: (filters = {}) => run('/movimientos/ingresos', filters),
 
-  fetchGastos: async (filters = {}) => {
-    set({ isLoading: true })
-    try {
-      const { data } = await api.get('/movimientos/gastos', { params: filters })
-      set({ movimientos: data, isLoading: false })
-    } catch (err) {
-      set({ isLoading: false })
-      throw err
-    }
-  },
+  fetchGastos: (filters = {}) => run('/movimientos/gastos', filters),
 
   createMovimiento: async (movimientoData) => {
     const { data } = await api.post('/movimientos', movimientoData)
@@ -60,6 +57,7 @@ const useExpenseStore = create((set) => ({
       movimientos: state.movimientos.filter((m) => m.id_movimiento !== id),
     }))
   },
-}))
+  }
+})
 
 export default useExpenseStore
